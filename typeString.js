4dbb6b69a79104df09b3af131240050ec6018963
mitodak <lucasbsilva59@gmail.com>
2022-10-07 12:33:03 -0300

    iniciando os trabalhos

---
+++ typeString.js
@@ -0,0 +1,31 @@
+const txt1 = "Hello"
+const txt2 = 'mundo'
+
+
+const citacao = 'olá ' + txt2;
+console.log(citacao)
+
+const cifrao = '\u0025'
+const aMaiusculo = '\u0041'
+const tique = '\u3052'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+
+// Aumento de caractere 
+
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+
+//console.log(cidade === input); // false
+const nome = 'luis'; 
+{
+const nome = "lucas"
+
+console.log(nome);
+}
