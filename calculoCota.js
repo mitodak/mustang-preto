87390b2a42f73dc946552fe541fb413a26af60cc
mitodak <lucasbsilva59@gmail.com>
2022-10-10 14:05:37 -0300

    cálculo cota 2.0

--- calculoCota.js
+++ calculoCota.js
@@ -3,11 +3,10 @@ const taxaAdm  = 24;
 const prazoGrp = 240;
 const percMens = credito / prazoGrp * 0.001;
 const percTaxa = taxaAdm / prazoGrp;
-const percTotal= percMens + percTaxa
+const percTotal= percMens + percTaxa;
 const vlrParc  = credito * percTotal * 0.01;
 const parcArred= parseFloat(vlrParc.toFixed(1));
 
-console.log("A taxa de adminstração é de", taxaAdm,"%. Sendo assim,")
+console.log("A taxa de adminstração é de", taxaAdm,"%.");
+console.log("Sendo assim, o valor da parcela é de R$", parcArred,".");
 
-console.log(vlrParc)
-console.log(parcArred)
