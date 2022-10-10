17d0b1990f8d4122042e05a7c0ea86c7642bc6da
mitodak <lucasbsilva59@gmail.com>
2022-10-10 17:07:47 -0300

    conversão

---
+++ conversão.js
@@ -0,0 +1,13 @@
+// tipo de dado
+//booleanos
+
+// conversão implicita
+
+const numero = 456;
+const numeroString = Number ("456A");
+
+console.log (numero == numeroString)
+
+// Conversão
+
+console.log (numero + numeroString)
