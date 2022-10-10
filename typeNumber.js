9f1cb8e7d1523b2ebd26ef8f6504fe43d24c3872
mitodak <lucasbsilva59@gmail.com>
2022-10-10 09:47:49 -0300

    First Commit

---
+++ typeNumber.js
@@ -0,0 +1,30 @@
+const meuNumero = 3;
+
+const primeiroNumero = 12;
+
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+
+console.log(operacaoMatematica);
+
+
+// ponto flutuante (float)
+
+const numFloat = 3.2;
+const numFloatSemZero = .5;
+
+
+const operacao2 = numFloat / numFloatSemZero
+
+console.log(operacao2);
+
+const nm = "lucas" //exemplo de NaN
+
+console.log(nm * segundoNumero); //exemplo de NaN
+
+
+const a = 10
+var b = 0
+console.log(a/b) // Infinity
