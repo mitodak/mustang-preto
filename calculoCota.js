8b563f90a0bd88fbb2903c3361590eb9fe406074
mitodak <lucasbsilva59@gmail.com>
2023-01-09 17:37:23 -0300

    Calculadora de Parcela

--- calculoCota.js
+++ calculoCota.js
@@ -2,10 +2,10 @@
 
 const credito     = 250000;                               // Crédito
 const taxaAdm     = 24;                                   // Taxa de Administração
+const flagSeguro  = true                                 // Define se a cota possui ou não seguro
 const prazoGrp    = 240;                                  // Prazo do grupo
 const percMens    = 100 / prazoGrp;                       // Percentual mensal sem T.A.
 const txSeguro    = 0.00038;                              // Percentual seguro
-const flagSeguro  = false                                 // Define se a cota possui ou não seguro
 const credComTA   = taxaAdm * 0.01 * credito + credito    // Define valor do crédito
 const percTaxa    = taxaAdm / prazoGrp;                   // Percentual da Taxa de Administração
 const percTotal   = percMens + percTaxa;                  // Percentual mensal com T.A.
@@ -17,7 +17,7 @@ const parcArredSg = parseFloat(vlrParcSeg.toFixed(2));    // Parcela formatada c
 
 if (flagSeguro === true) {
     console.log(
-`Sua cota possui seguro, através da categoria CRÉDITO + TAXA DE ADMINISTRAÇÃO.
+`Sua cota possui seguro, através da categoria CRÉDITO (R$${credito}) + TAXA DE ADMINISTRAÇÃO (${taxaAdm}%), sendo R$${credComTA} o valor de cobertura.
 Valor da parcel: R$${parcArredSg}`)
 } else {
     console.log(`Sua cota não possui seguro. Valor da parcel: R$${parcArred}`)
