e2987177d342a2f22433ff7ce9f6fa745ae56a9a
mitodak <lucasbsilva59@gmail.com>
2023-01-09 10:49:50 -0300

    template-string

---
+++ template-string.js
@@ -0,0 +1,10 @@
+const nome = "Lucas";
+const idade = 2023-2001;
+const cidadeNascimento = "Curitiba";
+// const apresentacao = "Meu nome é " + nome + ", tenho " + idade + " anos de idade e nasci na cidade de "
+//  + cidadeNascimento + "."
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos de idade e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
+
